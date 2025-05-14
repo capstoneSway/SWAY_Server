5f9b5d3e7f5bcd37f49f8eee8f63516a08d5b331
Dohyeon Kim <kamarooni@naver.com>
2025-05-14 22:04:38 +0900

    board - profile_image 추가

--- board/serializers.py
+++ board/serializers.py
@@ -11,14 +11,15 @@ class CommentSerializer(serializers.ModelSerializer):
     board_id = serializers.IntegerField(source='board.id', read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
     nickname = serializers.CharField(source='user.nickname', read_only=True)
+    profile_image = serializers.URLField(source='user.profile_image', read_only=True)
     parent_username = serializers.CharField(source='parent_user.username', read_only=True)
     parent_nickname = serializers.CharField(source='parent_user.nickname', read_only=True)
     like_count = serializers.SerializerMethodField()
     class Meta:
         model = Comment
-        fields = ('id', 'username', 'nickname', 'board_id', 'date', 'parent_id', 'parent_username', 'parent_nickname', 'content', 'like_count', 'reply')
+        fields = ('id', 'username', 'nickname', 'profile_image', 'board_id', 'date', 'parent_id', 'parent_username', 'parent_nickname', 'content', 'like_count', 'reply')
         read_only_fields = [
-            'id', 'user', 'username', 'nickname', 'board_id',
+            'id', 'user', 'username', 'nickname', 'profile_image', 'board_id',
             'date', 'parent_username', 'parent_nickname',
             'like_count', 'reply'
         ]
@@ -44,13 +45,14 @@ class CommentSerializer(serializers.ModelSerializer):
 # 디테일 페이지에서 재귀문 제거를 위해 따로 작성 / 읽기전용
 class CommentDetailSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
+    profile_image = serializers.URLField(source='user.profile_image', read_only=True)
     board_id = serializers.IntegerField(source='board.id', read_only=True)
     parent_nickname = serializers.CharField(source='parent_user.nickname', read_only=True)
     class Meta:
         model = Comment
-        fields = ('id', 'username', 'board_id' ,'date', 'parent_id', 'parent_nickname', 'content')
+        fields = ('id', 'username', 'profile_image', 'board_id' ,'date', 'parent_id', 'parent_nickname', 'content')
         read_only_fields = [
-            'id', 'user', 'username', 'nickname', 'board_id',
+            'id', 'user', 'username', 'nickname', 'profile_image', 'board_id',
             'date', 'parent_nickname']
 
 class BoardSerializer(serializers.ModelSerializer):
@@ -58,11 +60,12 @@ class BoardSerializer(serializers.ModelSerializer):
     like_count = serializers.SerializerMethodField()
     scrap_count = serializers.SerializerMethodField()
     nickname = serializers.CharField(source='user.nickname', read_only=True)
-
+    profile_image = serializers.URLField(source='user.profile_image', read_only=True)
+    
     class Meta:
         model = Board
-        fields = ['id', 'username', 'nickname', 'title', 'content', 'image', 'date', 'like_count', 'scrap_count']
-        read_only_fields = ['id', 'user', 'username', 'nickname', 'date', 'like_count', 'scrap_count']
+        fields = ['id', 'username', 'nickname', 'profile_image', 'title', 'content', 'image', 'date', 'like_count', 'scrap_count']
+        read_only_fields = ['id', 'user', 'username', 'nickname', 'profile_image', 'date', 'like_count', 'scrap_count']
     
     def get_like_count(self, obj):
         return obj.likes.count()
