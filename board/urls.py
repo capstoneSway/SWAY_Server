7af71aeff691d1fa302977b1dac832598e48021a
Dohyeon Kim <kamarooni@naver.com>
2025-05-25 18:22:31 +0900

    fix: board block 기능 url 통일

--- board/urls.py
+++ board/urls.py
@@ -9,14 +9,16 @@ urlpatterns = [
     path('', BoardList.as_view(), name='board-list'),
     path('create/', BoardCreate.as_view(), name='board-create'),
     path('<int:pk>/', BoardDetail.as_view(),name='board-detail'),
-    path('<int:pk>/block/', AddBlockUserView.as_view(),name='block-board-user'),
+    path('<int:pk>/block-user/', AddBlockUserView.as_view(),name='block-board-user'),
     path('<int:board_id>/comments/', CommentList.as_view(), name='comment-list-create'),
     path('<int:board_id>/update/', BoardUpdate.as_view(), name='board-update'),
     path('<int:board_id>/comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
-    path('<int:board_id>/comments/<int:pk>/block/', AddBlockCommentUserView.as_view(), name='block-comment-user'),
+    path('<int:board_id>/comments/<int:pk>/block-user/', AddBlockCommentUserView.as_view(), name='block-comment-user'),
     path('<int:board_id>/like/', BoardLikeToggleView.as_view()),
     path('<int:board_id>/comments/<int:comment_id>/like/', CommentLikeToggleView.as_view()),
     path('<int:board_id>/scrap/', BoardScrapToggleView.as_view()),
     path('<int:board_id>/report/', BoardReportView.as_view(), name='board-report'),
     path('<int:board_id>/comments/<int:comment_id>/report/', CommentReportView.as_view(), name='comment-report'),
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
