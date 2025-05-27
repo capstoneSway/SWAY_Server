62a66bf0eea2f4c9ae0bf33023fd7162f045a84d
YEJI KIM <ex_g@hufs.ac.kr>
2025-05-28 01:34:28 +0900

    fix: re) lightning serializer2

--- lightning/serializers.py
+++ lightning/serializers.py
@@ -8,7 +8,7 @@ class LightningSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lightning
         fields = ['id', 'host', 'title', 'created_at', 'end_time', 'participants', 'current_participant']
-        read_only_fields = ['id', 'host', 'created_at', 'current_participant_num']
+        read_only_fields = ['id', 'host', 'created_at', 'current_participant']
 
 class LightningDetailSerializer(serializers.ModelSerializer):
     host = serializers.StringRelatedField(read_only=True)
@@ -17,5 +17,5 @@ class LightningDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lightning
         fields = '__all__'
-        read_only_fields = ['id', 'host', 'created_at', 'like', 'current_participant_num']
+        read_only_fields = ['id', 'host', 'created_at', 'like', 'current_participant']
 
