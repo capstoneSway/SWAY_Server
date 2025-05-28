baf16e60dc570b3733e3b289e0a469b9d6f57bb0
Jaeyeon Lee <bestfourteen14@gmail.com>
2025-05-28 14:33:18 +0900

    fix: 원격 redis 연결 및 배포 변경

--- SWAY_back/settings.py
+++ SWAY_back/settings.py
@@ -131,7 +131,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('localhost', 6379)],
+            "hosts": [(os.getenv("REDIS_HOST", "localhost"), int(os.getenv("REDIS_PORT", 6379)))],
         },
     },
 }
