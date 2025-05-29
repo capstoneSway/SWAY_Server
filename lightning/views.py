9708a10bc55a40f2f9906242c38fa4acc4331012
YEJI KIM <ex_g@hufs.ac.kr>
2025-05-29 16:48:02 +0900

    fix: noti updated

--- lightning/views.py
+++ lightning/views.py
@@ -133,19 +133,19 @@ class JoinLightning(APIView):
             participation.save()
 
         # 알림 생성: 참가자가 번개 모임에 참가했음을 호스트에게 알림
-        # Notification.objects.create(
-        #     user = lightning.host,
-        #     type = "번개모임",
-        #     event = f"{user.username}님이 [{lightning.title}] 번개에 참가했어요."
-        # )
+        Notification.objects.create(
+            user = lightning.host,
+            type = "번개모임",
+            event = f"{user.username}님이 [{lightning.title}] 번개에 참가했어요."
+        )
 
         # 푸시 알림 전송: 호스트에게
-        # if lightning.host.fcm_token:
-        #     send_fcm_notification(
-        #         token=lightning.host.fcm_token,
-        #         title="번개 참가 알림",
-        #         body=f"{user.username}님이 [{lightning.title}] 번개에 참가했어요."
-        #     )
+        if lightning.host.fcm_token:
+            send_fcm_notification(
+                token=lightning.host.fcm_token,
+                title="번개 참가 알림",
+                body=f"{user.username}님이 [{lightning.title}] 번개에 참가했어요."
+            )
 
         participants = lightning.participants.all()
         serialized_participants = ParticipantSerializer(participants, many=True).data
@@ -183,20 +183,20 @@ class LeaveLightning(APIView):
             participation.save()
 
         # 알림 : 호스트에게 참가 취소 알림
-        # Notification.objects.create(
-        #     user=lightning.host,
-        #     type='번개모임',
-        #     event=lightning,
-        #     message=f"{user.username}님이 [{lightning.title}] 번개 참가를 취소했어요.",
-        # )
+        Notification.objects.create(
+            user=lightning.host,
+            type='번개모임',
+            event=lightning,
+            message=f"{user.username}님이 [{lightning.title}] 번개 참가를 취소했어요.",
+        )
 
         # 푸시 알림 전송: 호스트에게
-        # if lightning.host.fcm_token:
-        #     send_fcm_notification(
-        #         token=lightning.host.fcm_token,
-        #         title="번개 참가 취소 알림",
-        #         body=f"{user.username}님이 [{lightning.title}] 번개 참가를 취소했어요."
-        #     )
+        if lightning.host.fcm_token:
+            send_fcm_notification(
+                token=lightning.host.fcm_token,
+                title="번개 참가 취소 알림",
+                body=f"{user.username}님이 [{lightning.title}] 번개 참가를 취소했어요."
+            )
 
         participants = lightning.participants.all()
         serialized_participants = ParticipantSerializer(participants, many=True).data
