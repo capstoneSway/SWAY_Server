8d52883c7ec1ca8b60a0eb325bbb9b4231eb7382
YEJI KIM <ex_g@hufs.ac.kr>
2025-06-02 22:48:29 +0900

    fix: lightning serializer exp3

--- lightning/serializers.py
+++ lightning/serializers.py
@@ -5,18 +5,18 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class LightningParticipationSerializer(serializers.ModelSerializer):
-    relation_tag = serializers.CharField(source='get_relation_tag_display') 
+    relation_tag = serializers.CharField(source='lightningparticipation.relation_tag', source='get_relation_tag_display') 
 
     class Meta:
         model = LightningParticipation
         fields = ['relation_tag']
+
 
 class ParticipantSerializer(serializers.ModelSerializer):
     relation_tag = LightningParticipationSerializer(read_only=True)
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'profile_image', 'nationality', 'national_code', 'relation_tag']
-
 
 
 class LightningSerializer(serializers.ModelSerializer):
@@ -37,5 +37,3 @@ class LightningDetailSerializer(serializers.ModelSerializer):
         model = Lightning
         fields = '__all__'
         read_only_fields = ['id', 'host', 'created_at', 'like', 'current_participant']
-
-
